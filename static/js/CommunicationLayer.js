e7f15805aa899fea3d491d6c3ee8bec99796beff
Tanner Hobson <thobson2@vols.utk.edu>
2019-08-30 16:05:35 -0500

    Add initial LTBNET module and communication layer

--- static/js/CommunicationLayer.js
+++ static/js/CommunicationLayer.js
@@ -1,77 +1,93 @@
-function renderTopology(canvas, { size, bounds, project, needsUpdate }) {
-	console.log('render');
+function renderCommunication(canvas, { size, bounds, project, needsProjectionUpdate }) {
 	const context = this._context;
 	if (!context) return;
-	const SysParam = context.SysParam;
-	if (!SysParam) return;
-	const Bus = SysParam.Bus;
-	const Line = SysParam.Line;
+	const LTBNET_params = context.LTBNET_params;
+	if (!LTBNET_params) return;
+	const { Link } = LTBNET_params;
+	const { Pdc } = LTBNET_params;
+	const { Pmu } = LTBNET_params;
+	const { Switch } = LTBNET_params;
 
-	console.log('render', { needsUpdate });
+	let paramCache = this._cache.get(LTBNET_params);
+	if (!paramCache) {
+		paramCache = {};
+		this._cache.set(LTBNET_params, paramCache);
+	}
 
-	let busCache = this._cache.get(Bus);
-	let { busPixelCoords, busLookup } = busCache ? busCache : {};
-	if (!busCache || needsUpdate) {
-		busCache = {};
-		this._cache.set(Bus, busCache);
-
-		busPixelCoords = busCache.busPixelCoords =
-			new NDArray([Bus.shape[0], 2]);
-	
-		for (let i=0; i<Bus.shape[0]; ++i) {
-			const lat = Bus.get(i, 6);
-			const lng = Bus.get(i, 7);
+	let { pdcPixelCoords } = paramCache;
+	if (!pdcPixelCoords || needsProjectionUpdate) {
+		pdcPixelCoords = paramCache.pdcPixelCoords = new NDArray('C', [Pdc.shape[0], 2]);
+		for (let i=0; i<Pdc.shape[0]; ++i) {
+			const lat = Pdc.get(i, 0);
+			const lng = Pdc.get(i, 1);
 			const point = project(L.latLng(lat, lng));
-			busPixelCoords.set(point.x, i, 0);
-			busPixelCoords.set(point.y, i, 1);
+			pdcPixelCoords.set(point.x, i, 0);
+			pdcPixelCoords.set(point.y, i, 1);
 		}
+	}
 
-		busLookup = busCache.busLookup =
-			new Map();
-		for (let i=0; i<Bus.shape[0]; ++i) {
-			const num = Bus.get(i, 0);
-			busLookup.set(num, i);
+	let { pmuPixelCoords } = paramCache;
+	if (!pmuPixelCoords || needsProjectionUpdate) {
+		pmuPixelCoords = paramCache.pmuPixelCoords = new NDArray('C', [Pmu.shape[0], 2]);
+		for (let i=0; i<Pmu.shape[0]; ++i) {
+			const lat = Pmu.get(i, 0);
+			const lng = Pmu.get(i, 1);
+			const point = project(L.latLng(lat, lng));
+			pmuPixelCoords.set(point.x, i, 0);
+			pmuPixelCoords.set(point.y, i, 1);
+		}
+	}
+
+	let { switchPixelCoords } = paramCache;
+	if (!switchPixelCoords || needsProjectionUpdate) {
+		switchPixelCoords = paramCache.switchPixelCoords = new NDArray('C', [Switch.shape[0], 2]);
+		for (let i=0; i<Switch.shape[0]; ++i) {
+			const lat = Switch.get(i, 0);
+			const lng = Switch.get(i, 1);
+			const point = project(L.latLng(lat, lng));
+			switchPixelCoords.set(point.x, i, 0);
+			switchPixelCoords.set(point.y, i, 1);
 		}
 	}
 
 	const ctx = canvas.getContext('2d');
 	ctx.clearRect(0, 0, size.x, size.y);
 
-	ctx.fillStyle = 'black';
-	for (let i=0; i<Bus.shape[0]; ++i) {
-		const x = busPixelCoords.get(i, 0);
-		const y = busPixelCoords.get(i, 1);
+	ctx.fillStyle = 'red';
+	for (let i=0; i<Pdc.shape[0]; ++i) {
+		const x = pdcPixelCoords.get(i, 0);
+		const y = pdcPixelCoords.get(i, 1);
 		ctx.beginPath();
 		ctx.arc(x, y, 10.0, 0, 2 * Math.PI);
 		ctx.fill();
 	}
 
-	ctx.lineWidth = 5;
-	ctx.beginPath();
-	for (let i=0; i<Line.shape[0]; ++i){
-		const nodeFrom = Line.get(i, 0);
-		const nodeFromX = busPixelCoords.get(busLookup.get(nodeFrom), 0);
-		const nodeFromY = busPixelCoords.get(busLookup.get(nodeFrom), 1);
-
-		const nodeTo = Line.get(i, 1);
-		const nodeToX = busPixelCoords.get(busLookup.get(nodeTo), 0);
-		const nodeToY = busPixelCoords.get(busLookup.get(nodeTo), 1);
-
-		ctx.moveTo(nodeFromX, nodeFromY);
-		ctx.lineTo(nodeToX, nodeToY);
+	ctx.fillStyle = 'blue';
+	for (let i=0; i<Pmu.shape[0]; ++i) {
+		const x = pmuPixelCoords.get(i, 0);
+		const y = pmuPixelCoords.get(i, 1);
+		ctx.beginPath();
+		ctx.arc(x, y, 10.0, 0, 2 * Math.PI);
+		ctx.fill();
 	}
-	ctx.closePath();
-	ctx.stroke();
+
+	ctx.fillStyle = 'green';
+	for (let i=0; i<Switch.shape[0]; ++i) {
+		const x = switchPixelCoords.get(i, 0);
+		const y = switchPixelCoords.get(i, 1);
+		ctx.beginPath();
+		ctx.arc(x, y, 10.0, 0, 2 * Math.PI);
+		ctx.fill();
+	}
 }
 
-L.TopologyLayer = L.CanvasLayer.extend({
+L.CommunicationLayer = L.CanvasLayer.extend({
 	options: {
-		render: renderTopology,
+		render: renderCommunication,
 	},
 
 	initialize(options) {
 		this._context = null;
-		this._Bus = new WeakMap();
 		this._cache = new WeakMap();
 		L.CanvasLayer.prototype.initialize.call(this, options);
 	},
@@ -82,6 +98,6 @@ L.TopologyLayer = L.CanvasLayer.extend({
 	},
 });
 
-L.topologyLayer = function(options) {
-	return new L.TopologyLayer();
+L.communicationLayer = function(options) {
+	return new L.CommunicationLayer(options);
 };
