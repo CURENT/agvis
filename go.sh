a4f50f06116ab349e41f42b71f8b0dfadc850093
Hantao Cui <hcui7@utk.edu>
2019-11-20 12:56:49 -0500

    Reverted changes on macos

--- go.sh
+++ go.sh
@@ -49,22 +49,25 @@ run() {
 		$tag "$@"
 }
 
-run_nohost() {
-	if [ -n "$xauth" ]; then
-		rm -f $xauth
-		xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $xauth nmerge -
-	fi
-	docker run --rm \
-		${interactive:+-it} \
-		${script:+-a stdin -a stdout -a stderr --sig-proxy=true} \
-		${ipc:+--ipc=$ipc} \
-		${user:+-u $(id -u):$(id -g)} \
-		${cwd:+-v $PWD:$PWD -w $PWD} \
-		${port:+-p $port:$port} \
-		${data:+-v $data:$data} \
-		${xauth:+-e DISPLAY -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -v /etc/shadow:/etc/shadow:ro -v /etc/sudoers.d:/etc/sudoers.d:ro -v $xauth:$xauth -e XAUTHORITY=$xauth} \
-		${entrypoint:+--entrypoint $entrypoint} \
-		$tag "$@"
+run_8810() {
+    port=8810
+    net=
+    run "$@"
+}
+run_8811() {
+    port=8811
+    net=host
+    run "$@"
+}
+run_8812() {
+    port=8812
+    net=host
+    run "$@"
+}
+run_8819() {
+    port=8819
+    net=
+    run "$@"
 }
 
 inspect() {
@@ -126,10 +129,10 @@ dev() {
 	tmux split-window -v
 	tmux split-window -v
 	tmux select-layout tiled
-	tmux send-keys -t1 "./go.sh dime tcp://127.0.0.1:$((port+9))" Enter
-	tmux send-keys -t2 "./go.sh run_nohost python3.7 server.py --port $((port+0)) --bind 0.0.0.0" Enter
-	tmux send-keys -t3 "./go.sh python wsdime.py --port $((port+1)) --dhost tcp://127.0.0.1:$((port+9))" Enter
-	tmux send-keys -t4 "./go.sh python wsdime.py --port $((port+2)) --dhost tcp://127.0.0.1:$((port+9)) --name geovis2" Enter
+	tmux send-keys -t1 "./go.sh run_8819 dime --debug tcp://0.0.0.0:$((port+9))" Enter
+	tmux send-keys -t2 "./go.sh run_8810 python3.7 server.py --port $((port+0)) --bind 0.0.0.0" Enter
+	tmux send-keys -t3 "./go.sh run_8811 python3.7 wsdime.py --port $((port+1)) --dhost tcp://127.0.0.1:$((port+9))" Enter
+	tmux send-keys -t4 "./go.sh run_8812 python3.7 wsdime.py --port $((port+2)) --dhost tcp://127.0.0.1:$((port+9)) --name geovis2" Enter
 	tmux send-keys -t5 "./go.sh andes --routine=tds /opt/andes/cases/ieee14/ieee14_syn.dm --dime=tcp://127.0.0.1:$((port+9))"
 }
 
