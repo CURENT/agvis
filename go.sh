9e8f572b5562edf5a520e2ed968c4061f0db5486
Nicholas West <nwest13@vols.utk.edu>
2020-09-02 14:39:00 -0400

    Updating for bugfix

--- go.sh
+++ go.sh
@@ -132,7 +132,7 @@ dev() {
 	tmux select-layout tiled
 	tmux send-keys -t0 "docker run -t -v /tmp:/tmp -p 8818:8818 $tag dime -vv -l unix:/tmp/dime2 -l ws:$((port+8))" Enter
 	tmux send-keys -t1 "./go.sh run_8810 python3 server.py --port $((port+0)) --bind 0.0.0.0" Enter
-	tmux send-keys -t2 "docker run -t -v /tmp:/tmp $tag andes -v 10 run /home/cui/wecc_vis.xlsx -r tds --qrt --kqrt=1"
+	tmux send-keys -t2 "docker run -t -v /tmp:/tmp $tag andes -v 10 run /home/cui/wecc_vis.xlsx -r tds"
 }
 
 "$@"
