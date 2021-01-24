37c2aa542f8ecaa34b08298b16302ad366f039f5
Nicholas West <nwest13@vols.utk.edu>
2021-01-24 17:22:44 -0500

    Added ability to change labels via URL

--- static/js/SidebarConfig.js
+++ static/js/SidebarConfig.js
@@ -1,15 +1,15 @@
 const table_html = `
 <table style="width: 100%;">
     <tr>
-        <td>V Angle (rad) min/max</td>
+        <td><span name="opt_alabel">V Angle (rad) min/max</span></td>
         <td style="white-space: nowrap;"><input type="text" name="opt_amin" pattern="[0-9]*(\.[0-9]*)?" size="7"> - <input type="text" name="opt_amax" pattern="[0-9]*(\.[0-9]*)?" size="7"></td>
     </tr>
     <tr>
-        <td>V Magnitude (p.u.) min/max</td>
+        <td><span name="opt_vlabel">V Magnitude (p.u.) min/max</span></td>
         <td style="white-space: nowrap;"><input type="text" name="opt_vmin" pattern="[0-9]*(\.[0-9]*)?" size="7"> - <input type="text" name="opt_vmax" pattern="[0-9]*(\.[0-9]*)?" size="7"></td>
     </tr>
     <tr>
-        <td>Frequency (p.u.) min/max</td>
+        <td><span name="opt_flabel">Frequency (p.u.) min/max</span></td>
         <td style="white-space: nowrap;"><input type="text" name="opt_fmin" pattern="[0-9]*(\.[0-9]*)?" size="7"> - <input type="text" name="opt_fmax" pattern="[0-9]*(\.[0-9]*)?" size="7"></td>
     </tr>
     <tr>
@@ -31,6 +31,8 @@ const table_html = `
 </div>
 `
 
+const SIDEBAR_CALLBACKS = [];
+
 function addSidebarConfig(num, options, map, layers, sidebar) {
     const table_id = "configpanel" + num;
 
@@ -43,8 +45,67 @@ function addSidebarConfig(num, options, map, layers, sidebar) {
     });
 
     const opt_amin = document.querySelector(`#${table_id} input[name='opt_amin']`);
+    const opt_amax = document.querySelector(`#${table_id} input[name='opt_amax']`);
+    const opt_vmin = document.querySelector(`#${table_id} input[name='opt_vmin']`);
+    const opt_vmax = document.querySelector(`#${table_id} input[name='opt_vmax']`);
+    const opt_fmin = document.querySelector(`#${table_id} input[name='opt_fmin']`);
+    const opt_fmax = document.querySelector(`#${table_id} input[name='opt_fmax']`);
+    const opt_togglezones = document.querySelector(`#${table_id} input[name='opt_togglezones']`);
+    const opt_togglebuslabels = document.querySelector(`#${table_id} input[name='opt_togglebuslabels']`);
 
-    opt_amin.value = options["amin" + num];
+    const opt_loadconfig = document.querySelector(`#${table_id} input[name='opt_loadconfig']`);
+    const opt_saveconfig = document.querySelector(`#${table_id} input[name='opt_saveconfig']`);
+
+    const opt_alabel = document.querySelector(`#${table_id} span[name='opt_alabel']`);
+    const opt_vlabel = document.querySelector(`#${table_id} span[name='opt_vlabel']`);
+    const opt_flabel = document.querySelector(`#${table_id} span[name='opt_flabel']`);
+
+    function updateInputs() {
+        if ("amin" + num in options) {
+            opt_amin.value = options["amin" + num];
+        }
+
+        if ("amax" + num in options) {
+            opt_amax.value = options["amax" + num];
+        }
+
+        if ("vmin" + num in options) {
+            opt_vmin.value = options["vmin" + num];
+        }
+
+        if ("vmax" + num in options) {
+            opt_vmax.value = options["vmax" + num];
+        }
+
+        if ("fmin" + num in options) {
+            opt_fmin.value = options["fmin" + num];
+        }
+
+        if ("fmax" + num in options) {
+            opt_fmax.value = options["fmax" + num];
+        }
+
+        if ("togglezones" + num in options) {
+            opt_togglezones.checked = options["togglezones" + num];
+        }
+
+        if ("togglebuslabels" + num in options) {
+            opt_togglebuslabels.checked = options["togglebuslabels" + num];
+        }
+
+        if ("alabel" + num in options) {
+            opt_alabel.innerHTML = options["alabel" + num] + " min/max";
+        }
+
+        if ("vlabel" + num in options) {
+            opt_vlabel.innerHTML = options["vlabel" + num] + " min/max";
+        }
+
+        if ("flabel" + num in options) {
+            opt_flabel.innerHTML = options["flabel" + num] + " min/max";
+        }
+    };
+
     opt_amin.oninput = function() {
         const val = Number(opt_amin.value);
 
@@ -63,9 +124,6 @@ function addSidebarConfig(num, options, map, layers, sidebar) {
         }
     }
 
-    const opt_amax = document.querySelector(`#${table_id} input[name='opt_amax']`);
-
-    opt_amax.value = options["amax" + num];
     opt_amax.oninput = function() {
         const val = Number(opt_amax.value);
 
@@ -84,9 +142,6 @@ function addSidebarConfig(num, options, map, layers, sidebar) {
         }
     };
 
-    const opt_vmin = document.querySelector(`#${table_id} input[name='opt_vmin']`);
-
-    opt_vmin.value = options["vmin" + num];
     opt_vmin.oninput = function() {
         const val = Number(opt_vmin.value);
 
@@ -105,9 +160,6 @@ function addSidebarConfig(num, options, map, layers, sidebar) {
         }
     };
 
-    const opt_vmax = document.querySelector(`#${table_id} input[name='opt_vmax']`);
-
-    opt_vmax.value = options["vmax" + num];
     opt_vmax.oninput = function() {
         const val = Number(opt_vmax.value);
 
@@ -126,9 +178,6 @@ function addSidebarConfig(num, options, map, layers, sidebar) {
         }
     }
 
-    const opt_fmin = document.querySelector(`#${table_id} input[name='opt_fmin']`);
-
-    opt_fmin.value = options["fmin" + num];
     opt_fmin.oninput = function() {
         const val = Number(opt_fmin.value);
 
@@ -147,9 +196,6 @@ function addSidebarConfig(num, options, map, layers, sidebar) {
         }
     };
 
-    const opt_fmax = document.querySelector(`#${table_id} input[name='opt_fmax']`);
-
-    opt_fmax.value = options["fmax" + num];
     opt_fmax.oninput = function() {
         const val = Number(opt_fmax.value);
 
@@ -168,45 +214,62 @@ function addSidebarConfig(num, options, map, layers, sidebar) {
         }
     };
 
-    const opt_togglezones = document.querySelector(`#${table_id} input[name='opt_togglezones']`);
-
     opt_togglezones.onclick = function() {
         layers.zoneLayer.toggleRender();
-        opt_togglezones.checked = layers1.zoneLayer._render;
-    };
 
-    const opt_togglebuslabels = document.querySelector(`#${table_id} input[name='opt_togglebuslabels']`);
+        const val = layers.zoneLayer._render;
+
+        opt_togglezones.checked = val;
+        options["togglezones" + num] = val;
+
+        let dt = new Date();
+        dt.setTime(dt.getTime() + (365 * 24 * 60 * 60 * 1000));
+        dt = dt.toUTCString();
+
+        document.cookie = `togglezones${num}=${val};expires=${dt};path=/`;
+    };
 
     opt_togglebuslabels.onclick = function() {
-        layers.topologyLayer._render_bus_ids = opt_togglebuslabels.checked;
+        const val = opt_togglebuslabels.checked;
+
+        layers.topologyLayer._render_bus_ids = val;
         layers.topologyLayer.redraw();
+        options["togglebuslabels" + num] = val;
+
+        let dt = new Date();
+        dt.setTime(dt.getTime() + (365 * 24 * 60 * 60 * 1000));
+        dt = dt.toUTCString();
+
+        document.cookie = `togglebuslabels${num}=${val};expires=${dt};path=/`;
     };
 
-    const opt_loadconfig = document.querySelector(`#${table_id} input[name='opt_loadconfig']`);
     const opt_loadconfig_input = document.createElement("input");
 
     opt_loadconfig_input.style.display = "none";
     opt_loadconfig_input.type = "file";
     document.body.appendChild(opt_loadconfig_input);
 
-    opt_loadconfig.onclick = function() {
-        opt_loadconfig_input.onchange = function() {
-            if (opt_loadconfig_input.files.length > 0) {
-                let fr = new FileReader();
+    opt_loadconfig_input.onchange = function() {
+        if (opt_loadconfig_input.files.length > 0) {
+            let fr = new FileReader();
 
-                fr.onload = function(file) {
-                    let newoptions = JSON.parse(file.target.result);
-                    Object.assign(options, newoptions);
+            fr.onload = function(file) {
+                let newoptions = JSON.parse(file.target.result);
+                Object.assign(options, newoptions);
+
+                for (let func of SIDEBAR_CALLBACKS) {
+                    func();
                 }
-
-                fr.readAsText(opt_loadconfig_input.files[0]);
             }
-        };
 
+            fr.readAsText(opt_loadconfig_input.files[0]);
+        }
+    };
+
+    opt_loadconfig.onclick = function() {
         opt_loadconfig_input.click();
-    }
+    };
 
-    const opt_saveconfig = document.querySelector(`#${table_id} input[name='opt_saveconfig']`);
     const opt_saveconfig_a = document.createElement("a");
 
     opt_saveconfig_a.style.display = "none";
@@ -221,4 +284,7 @@ function addSidebarConfig(num, options, map, layers, sidebar) {
 
         opt_saveconfig_a.click();
     }
+
+    updateInputs();
+    SIDEBAR_CALLBACKS.push(updateInputs);
 }
