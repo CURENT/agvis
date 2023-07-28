b05d3c57fe2144d1732faa75391301e0bed4cf88
zmalkmus <zmalkmus@vols.utk.edu>
2023-07-28 14:58:42 -0400

    moved order of operations

--- agvis/app.py
+++ agvis/app.py
@@ -7,16 +7,6 @@ import requests
 app = Flask(__name__)
 app.requests_session = requests.Session()
 
-# Serve index.html
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-# Serve static files
-@app.route('/<path:path>', methods=['GET'])
-def static_proxy(path):
-    return send_from_directory('static', path)
-
 def run_app(app_module, host='localhost', port=8810, workers=1):
     try:
         # Print out the URL to access the application
@@ -39,6 +29,16 @@ def run_app(app_module, host='localhost', port=8810, workers=1):
         print('\nAGVis has been stopped. You may now close the browser.')
     except Exception as e:
         print(f'An unexpected error has occured while trying to start AGVis: {e}')
+
+# Serve index.html
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Serve static files
+@app.route('/<path:path>', methods=['GET'])
+def static_proxy(path):
+    return send_from_directory('static', path)
 
 # Run the application
 if __name__ == '__main__':
