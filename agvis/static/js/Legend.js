5d14d27ece7f36105fcb134e4df92e4a422e4996
zmalkmus <zmalkmus@vols.utk.edu>
2023-08-25 08:30:37 -0400

    legend

--- agvis/static/js/Legend.js
+++ agvis/static/js/Legend.js
@@ -1,9 +1,13 @@
 L.Legend = L.Control.extend({
     onAdd: function() {
-        this.simulation_time = 0;
-        this.text = L.DomUtil.create('div');
-        this.text.innerHTML = "<p style=\"font-size:250%;\"><strong>Hello there</strong></p>";
-        return this.text;
+        let div = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
+
+        div.style.backgroundColor = "white";
+        div.style.boxSizing = "border-box";
+        div.style.padding = "4px";
+
+        div.innerHTML = "<p style=\"font-size:250%;\"><strong>Hello there</strong></p>";
+        return div;
     },
 
     onRemove: function() {
