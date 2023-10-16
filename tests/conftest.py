59700509f7e121906ddfce84135150e1ed42f415
zmalkmus <zmalkmus@vols.utk.edu>
2023-10-16 13:34:59 -0400

    basic test setup

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from agvis import run_app
+
+@pytest.fixture()
+def app():
+    app = run_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+import pytest
+import agvis
+
+# Items to test:
+#  1. Command line arguments
+#  2. DiME data?
+#  3. GET/POST Methods
+
+def test_app(client):
+    return True
