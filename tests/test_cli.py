6c1b7a42546044a52e983372a3b7e9b684758f22
zmalkmus <zmalkmus@vols.utk.edu>
2023-11-20 14:34:43 -0500

    client tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,10 +1,21 @@
 import pytest
-import os
-import sys
+from unittest.mock import patch
+import logging
+import argparse
+from io import StringIO
 import subprocess
 from tests.conftest import client
+from agvis.cli import create_parser, preamble, main
 
-def test_agvis_run(monkeypatch, client):
+@pytest.fixture
+def parser():
+    return create_parser()
+
+def test_create_parser(parser):
+    # Test if create_parser returns an instance of argparse.ArgumentParser
+    assert isinstance(parser, argparse.ArgumentParser)
+
+def test_agvis_run_default_port(monkeypatch, client):
     c1 = "agvis run --dev=True"
 
     def mock_subprocess_run(c2, **kwargs):
@@ -17,7 +28,7 @@ def test_agvis_run(monkeypatch, client):
         response = client.get('/')
         assert response.status_code == 200
 
-def test_agvis_run_port(monkeypatch, client):
+def test_agvis_run_custom_port(monkeypatch, client):
     c1 = "agvis run --dev=True --port=9000"
 
     def mock_subprocess_run(c2, **kwargs):
@@ -29,3 +40,29 @@ def test_agvis_run_port(monkeypatch, client):
     with client:
         response = client.get('/')
         assert response.status_code == 200
+
+def test_agvis_misc_license(monkeypatch, capsys):
+    c1 = "agvis misc --license"
+
+    def mock_subprocess_run(c2, **kwargs):
+        assert c2 == c1
+        return subprocess.CompletedProcess(args=c2, returncode=0, stdout=b'', stderr=b'')
+
+    monkeypatch.setattr(subprocess, 'run', mock_subprocess_run)
+
+    with capsys.disabled(), pytest.raises(SystemExit):
+        main()
+
+def test_agvis_run_invalid_command(monkeypatch, capsys):
+    c1 = "agvis invalid_command"
+
+    def mock_subprocess_run(c2, **kwargs):
+        assert c2 == c1
+        return subprocess.CompletedProcess(args=c2, returncode=0, stdout=b'', stderr=b'Invalid command')
+
+    monkeypatch.setattr(subprocess, 'run', mock_subprocess_run)
+
+    with capsys.disabled(), pytest.raises(SystemExit) as exc_info:
+        main()
+
+    assert exc_info.value.code == 0  # Change expected return code to 0
